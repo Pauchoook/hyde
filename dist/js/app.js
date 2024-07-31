817ac536fc3109ac00dfdf56a0090f1189fb087c
Pauchoook <nkolatarasov@mail.ru>
2024-07-31 20:16:47 +0300

    basket started

--- dist/js/app.js
+++ dist/js/app.js
@@ -7046,6 +7046,20 @@
             }));
         }));
     }
+    const navBasket = document.querySelectorAll(".basket__numbers-wrapper");
+    if (navBasket.length) navBasket.forEach((n => {
+        const btnMinus = n.querySelector(".btn-minus");
+        const btnPlus = n.querySelector(".btn-plus");
+        const currentNum = n.querySelector(".basket__product-number");
+        btnMinus.addEventListener("click", (() => {
+            const num = Number(currentNum.textContent);
+            if (num >= 1) currentNum.textContent = num - 1;
+        }));
+        btnPlus.addEventListener("click", (() => {
+            const num = Number(currentNum.textContent);
+            currentNum.textContent = num + 1;
+        }));
+    }));
     function balanceShow() {
         const btnShow = document.querySelector(".my-balance__btn-start");
         if (btnShow) btnShow.addEventListener("click", (() => {
--- src/js/files/numbers-busket.js
+++ src/js/files/numbers-busket.js
@@ -20,3 +20,25 @@ export default function numbersBasket() {
     });
   }
 }
+
+// страница корзины
+const navBasket = document.querySelectorAll(".basket__numbers-wrapper");
+if (navBasket.length) {
+  navBasket.forEach((n) => {
+    const btnMinus = n.querySelector(".btn-minus");
+    const btnPlus = n.querySelector(".btn-plus");
+    const currentNum = n.querySelector(".basket__product-number");
+
+    btnMinus.addEventListener("click", () => {
+      const num = Number(currentNum.textContent);
+      if (num >= 1) {
+        currentNum.textContent = num - 1;
+      }
+    });
+
+    btnPlus.addEventListener("click", () => {
+      const num = Number(currentNum.textContent);
+      currentNum.textContent = num + 1;
+    });
+  });
+}
